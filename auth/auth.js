284de8807d17d5b252dc54f01e6864d07a52519d
Bilab Thapa <thapabilab9@gmail.com>
2022-05-04 15:34:41 +0545

    First Commit

--- auth/auth.js
+++ auth/auth.js
@@ -1,10 +1,21 @@
 const jwt = require("jsonwebtoken");
 
-
 module.exports.customerGuard = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const data = jwt.verify(token,"bilab");
-    console.log(data);
-  } catch (e) {}
+    const data = jwt.verify(token, "bilab");
+    customer.findOne({_id:data.customerId})
+    .then((cdata)=>{
+        req.customerInfo = cdata;
+        next();
+    })
+    .catch((e)=>{
+        res.json({msg:'Invalid Token'})
+    })
+
+
+
+  } catch (e) {
+      res.json({msg:"Invalid Token"})
+  }
 };
